06d2dad1e4f2086293618c76093e9a844c28c7d1
murizzzz <muriithialvin3@gmail.com>
2026-05-28 17:49:30 +0300

    indes page changes

---
+++ assets/javascript/script.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                gold: {
+                    50: '#fff8e1',
+                    100: '#ffecb3',
+                    500: '#f6b800',
+                    600: '#d99a00',
+                    700: '#a86f00'
+                },
+                navy: '#07172f'
+            }
+        }
+    }
+}
